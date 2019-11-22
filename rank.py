0536b009f4e49731f2084a4b1a22faa83434551a
Hugh Cornett <cornetthugh@gmail.com>
2019-11-22 15:19:34 +0100

    new graph functions

--- rank.py
+++ rank.py
@@ -17,6 +17,10 @@ def newArrowGraph(n):
 def newDirectedGrid(n):
 	G = nx.DiGraph()
 
+	if n == 1:
+		G.add_node(1)
+		return G
+
 	for i in range(0, n):
 		for j in range(1, n+1):
 
@@ -38,11 +42,40 @@ def createlasso():
     })
     return G
 
+def createflower():
+    G = toGraph({
+    'A': ['B', 'E', 'F'],
+    'B': ['A','C', 'G'],
+    'C': ['B','D', 'H'],
+    'D': ['C','E', 'I'],
+    'E': ['A', 'D','J']
+    })
+    return G
+
 def createInwardStar():
     G = nx.DiGraph()
     for i in range(6):
         G.add_edge(str(i+1), 'A')
     return G
+
+def createSpam(f, n, p):
+	G = nx.DiGraph()
+
+	for i in range(f):
+		G.add_edge('T', 'f'+str(i+1))
+		G.add_edge('f'+str(i+1), 'T')
+	for j in range(n):
+		for k in range(p):
+			G.add_edge('n'+str(j+1), 'P'+str(k+1))
+			G.add_edge('P'+str(k+1), 'T')
+
+	for startNode in G:
+		if startNode[0] == 'n':
+			for endNode in G:
+				if endNode[0] == 'n' and endNode != startNode:
+					G.add_edge(startNode, endNode)
+
+	return G
 
 def indegree(G):
 
@@ -150,16 +183,38 @@ def compareRanking(dic1, dic2):
 				break
 	return distance
 
-G = newDirectedGrid(2)
+def testOnGraph(G):
+	hitsrank = hits(G, 100)
+	pagerank = pageRank(G, 100, 0.85)
+	indegreerank = indegree(G)
 
-hits = hits(G, 100)
-pagerank = pageRank(G, 100, 0.85)
-indegree = indegree(G)
+	print("HITS: "+str(rankDictionary(hitsrank)))
+	print("PageRank: "+str(rankDictionary(pagerank)))
+	print("InDegree: "+str(rankDictionary(indegreerank)))
 
-#print(rankDictionary(hits))
-#print(rankDictionary(pagerank))
-#print(rankDictionary(indegree))
+	print("HITS - PageRank: "+str(compareRanking(rankDictionary(hitsrank), rankDictionary(pagerank))))
+	print("HITS - InDegree: "+str(compareRanking(rankDictionary(hitsrank), rankDictionary(indegreerank))))
+	print("PageRank - InDegree: "+str(compareRanking(rankDictionary(pagerank), rankDictionary(indegreerank))))
 
-print("distance: "+str(compareRanking(rankDictionary(hits), rankDictionary(pagerank))))
 
+print("============")
+print("grid graph")
+testOnGraph(newDirectedGrid(3))
+print("============")
+print("arrow graph")
+testOnGraph(newArrowGraph(5))
+print("============")
+print("lasso graph")
+testOnGraph(createlasso())
+print("============")
+print("star graph")
+testOnGraph(createInwardStar())
+print("============")
+print("flower graph")
+testOnGraph(createflower())
+print("============")
+print("spam graph")
+testOnGraph(createSpam(3, 3, 1))
+
+G=createSpam(3,3,3)
 draw(G)
